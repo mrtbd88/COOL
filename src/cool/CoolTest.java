35ffee3477db55db130b9064823b1bccc1b769af
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-14 17:24:46 -0400

    adding feature branch

--- src/cool/CoolTest.java
+++ src/cool/CoolTest.java
@@ -1,10 +1,14 @@
 package cool;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CoolTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahfuj Tuhin\\eclipse-workspace\\COOL\\drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
 	}
 
 }
