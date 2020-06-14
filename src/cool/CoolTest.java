01712f5a91b8dffac264b005e849e821e2bb31d5
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-14 17:37:12 -0400

    done at last stage

--- src/cool/CoolTest.java
+++ src/cool/CoolTest.java
@@ -2,10 +2,13 @@ package cool;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class CoolTest {
 
-	public static void main(String[] args) {
+	@Test
+	public void openBrowser() {
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahfuj Tuhin\\eclipse-workspace\\COOL\\drivers\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.facebook.com/");
